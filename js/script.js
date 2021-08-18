055ca26927f5e0ca60c8941bf91c44877c609ebe
dienoov <alvinodien@yahoo.co.id>
2021-08-18 16:18:25 +0700

    Data update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 const loaded = () => {
     document.body.classList.add("loaded")
 
-    if (screen.width < screen.height) {
-        setTimeout(() => {
-            document.removeEventListener("scroll", aboutOnScroll);
-            about.classList.add("scroll");
-        }, 1500);
-    }
+    // if (screen.width < screen.height) {
+    //     setTimeout(() => {
+    //         document.removeEventListener("scroll", aboutOnScroll);
+    //         about.classList.add("scroll");
+    //     }, 1500);
+    // }
 };
 
 const navbar = document.getElementById("navbar");
@@ -28,45 +28,45 @@ const toggleNavbar = () => {
 };
 
 const about = document.getElementById("about");
-const aboutOnScroll = () => {
-    if (scrollY + screen.height > about.offsetTop + 144) {
+// const aboutOnScroll = () => {
+//     if (scrollY + screen.height > about.offsetTop + 360) {
         about.classList.add("scroll");
-    } else {
-        about.classList.remove("scroll");
-    }
-};
+//     } else {
+//         about.classList.remove("scroll");
+//     }
+// };
 
-const award = document.getElementById("award");
-const awardOnScroll = () => {
-    if (scrollY + screen.height > award.offsetTop + 144) {
+// const award = document.getElementById("award");
+// const awardOnScroll = () => {
+//     if (scrollY + screen.height > award.offsetTop + 360) {
         award.classList.add("scroll");
-    } else {
-        award.classList.remove("scroll");
-    }
-};
+//     } else {
+//         award.classList.remove("scroll");
+//     }
+// };
 
 const career = document.getElementById("career");
-const careerOnScroll = () => {
-    if (scrollY + screen.height > career.offsetTop + 144) {
+// const careerOnScroll = () => {
+//     if (scrollY + screen.height > career.offsetTop + 360) {
         career.classList.add("scroll");
-    } else {
-        career.classList.remove("scroll");
-    }
-};
+//     } else {
+//         career.classList.remove("scroll");
+//     }
+// };
 
 const work = document.getElementById("work");
-const workOnScroll = () => {
-    if (scrollY + screen.height > work.offsetTop + 144) {
+// const workOnScroll = () => {
+//     if (scrollY + screen.height > work.offsetTop + 360) {
         work.classList.add("scroll");
-    } else {
-        work.classList.remove("scroll");
-    }
-};
+//     } else {
+//         work.classList.remove("scroll");
+//     }
+// };
 
 window.addEventListener("load", loaded);
 document.addEventListener("scroll", navbarOnScroll);
-document.addEventListener("scroll", aboutOnScroll);
-document.addEventListener("scroll", awardOnScroll);
-document.addEventListener("scroll", careerOnScroll);
-document.addEventListener("scroll", workOnScroll);
+// document.addEventListener("scroll", aboutOnScroll);
+// document.addEventListener("scroll", awardOnScroll);
+// document.addEventListener("scroll", careerOnScroll);
+// document.addEventListener("scroll", workOnScroll);
 navbarToggler.addEventListener("click", toggleNavbar);
