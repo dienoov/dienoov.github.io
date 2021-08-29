4b8efbcb0705b06c38ea5f7da2349ef217118786
dienoov <alvinodien@yahoo.co.id>
2021-08-29 14:03:54 +0700

    About

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,7 @@ const heroTemplate = require('../views/components/hero.pug');
 const firstWorkTemplate = require('../views/components/works/first.pug');
 const secondWorkTemplate = require('../views/components/works/second.pug');
 const thirdWorkTemplate = require('../views/components/works/third.pug');
+const aboutTemplate = require('../views/components/about.pug');
 
 const main = document.body.querySelector('main');
 
@@ -11,6 +12,7 @@ main.insertAdjacentHTML('beforeend', heroTemplate());
 main.insertAdjacentHTML('beforeend', firstWorkTemplate());
 main.insertAdjacentHTML('beforeend', secondWorkTemplate());
 main.insertAdjacentHTML('beforeend', thirdWorkTemplate());
+main.insertAdjacentHTML('beforeend', aboutTemplate());
 
 main.addEventListener('wheel', (ev) => {
   main.scrollLeft += ev.deltaY;
